d7d0ff143ed628944b7d7b85dc9ed4e2e65fa6a1
i-ayo <iruoayo@gmail.com>
2026-01-23 10:35:27 +0000

    Changes to GeneralRepository class

--- ServerLibrary/Repositories/Implementations/GeneralDepartmentRepository.cs
+++ ServerLibrary/Repositories/Implementations/GeneralDepartmentRepository.cs
@@ -22,13 +22,15 @@ namespace ServerLibrary.Repositories.Implementations
         public async Task<List<GeneralDepartment>> GetAll() => await appDbContext.GeneralDepartments.ToListAsync();
         public async Task<GeneralDepartment> GetById(int id) => await appDbContext.GeneralDepartments.FindAsync(id);
 
-        public async Task<GeneralResponse> Insert(GeneralDepartment item)
-        {
-            if (!await CheckName(item.Name!)) return new GeneralResponse(false, "Department already added");
-            appDbContext.GeneralDepartments.Add(item);
-            await Commit();
-            return Success();
-        }
+            public async Task<GeneralResponse> Insert(GeneralDepartment item)
+            {
+                var checkIfNull = await CheckName(item.Name);
+                if (!checkIfNull)
+                return new GeneralResponse(false, "General Department already added");
+                appDbContext.GeneralDepartments.Add(item);
+                await Commit();
+                return Success();
+            }
 
         public async Task<GeneralResponse> Update(GeneralDepartment item)
         {
@@ -46,10 +48,9 @@ namespace ServerLibrary.Repositories.Implementations
 
         private async Task<bool> CheckName(string name)
         {
-            var item = await appDbContext.Departments.FirstOrDefaultAsync(x => x.Name!.ToLower().Equals(name.ToLower()));
-            return item is null;
+            return !await appDbContext.GeneralDepartments
+                .AnyAsync(x => x.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
         }
-
 
     }
 }
