4d80d6503491067dd861be6a752df7351a8e2888
jotakv <kevinjoel1995@gmail.com>
2026-03-27 16:29:24 -0400

    Set login properties as Required and redirect from Register to Login

--- BaseLibrary/DTOs/Login.cs
+++ BaseLibrary/DTOs/Login.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,9 @@ namespace BaseLibrary.DTOs
 {
     public class Login
     {
+        [Required]
         public string Email { get; set; } = string.Empty;
+        [Required]
         public string Password { get; set; } = string.Empty;
     }
 }
